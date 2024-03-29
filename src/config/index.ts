97d904573e15673e7ba08e04e80a7b329a43f1cb
Your Name <you@example.com>
2024-03-29 15:03:43 -0700

    Settings

---
+++ src/config/index.ts
@@ -0,0 +1,14 @@
+// src/config/index.ts
+
+export const ethereumConfig = {
+    network: 'ropsten', // or any other testnet
+    privateKey: 'YOUR_PRIVATE_KEY',
+    gasPrice: 20, // in Gwei
+    gasLimit: 21000,
+   };
+   
+   export const tablelandConfig = {
+    apiKey: 'YOUR_TABLELAND_API_KEY',
+    databaseId: 'YOUR_DATABASE_ID',
+   };
+   
