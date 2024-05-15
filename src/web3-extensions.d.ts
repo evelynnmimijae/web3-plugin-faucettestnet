cda85ee6d42e2f766abfb03311a842ecc3238e41
Your Name <you@example.com>
2024-05-14 19:16:56 -0700

    Updated interface ext and consistent naming

--- src/web3-extensions.d.ts
+++ src/web3-extensions.d.ts
@@ -1,9 +1,8 @@
-// web3-extensions.d.ts
 import { Web3 } from 'web3';
 import { FaucetPlugin } from './FaucetPlugin';
 
 declare module 'web3' {
- interface Web3 {
-    faucetPlugin: FaucetPlugin;
- }
+  interface Web3 {
+    faucet: FaucetPlugin;
+  }
 }
